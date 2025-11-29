44648eedb86b15829fbfd6a71cb6b1ad81513171
Piotr Moch <piotr@MacBook-Air-Piotr.local>
2025-11-29 23:00:15 +0100

    scr init

---
+++ src/main.py
@@ -0,0 +1,56 @@
+import datetime as dt
+import requests
+
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+API_KEY = open('key/api_key', 'r').read().strip()
+
+def city():
+    city_name = input("Choose city to check weather: ").strip()
+    if not city_name:
+        print("No city input!")
+        exit()
+    return city_name
+
+def kelvin_to_celsius_fahrenheit(kelvin):
+    celsius = kelvin - 273.15
+    fahrenheit = celsius * (9/5) + 32
+    return celsius, fahrenheit
+
+def get_weather(city: str):
+    params = {
+        "appid": API_KEY,
+        "q": city
+    }
+    response = requests.get(BASE_URL, params=params)
+
+    if response.status_code != 200:
+        raise Exception(f"API returned status {response.status_code}: {response.text}")
+
+    return response.json()
+
+def print_weather_data(data, city):
+    temp_kelvin = data['main']['temp']
+    temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp_kelvin)
+
+    feels_like_kelvin = data['main']['feels_like']
+    feels_like_celsius, feels_like_fahrenheit = kelvin_to_celsius_fahrenheit(feels_like_kelvin)
+
+    humidity = data['main']['humidity']
+    description = data['weather'][0]['description']
+
+    sunrise_time = dt.datetime.utcfromtimestamp(data['sys']['sunrise'] + data['timezone'])
+    sunset_time = dt.datetime.utcfromtimestamp(data['sys']['sunset'] + data['timezone'])
+
+    print(f"Temperature in {city}: {temp_celsius:.2f}°C or {temp_fahrenheit:.2f}°F")
+    print(f"Temperature in {city} feels like: {feels_like_celsius:.2f}°C or {feels_like_fahrenheit:.2f}°F")
+    print(f"Humidity in {city}: {humidity}%")
+    print(f"The weather in {city}: {description}")
+    print(f"The sunrise in {city}: {sunrise_time}")
+    print(f"The sunset in {city}: {sunset_time}")
+
+# tylko tutaj wywołujemy input i API
+if __name__ == "__main__":
+    CITY = city()
+    print(f"DEBUG: Checking weather for '{CITY}'")
+    weather_data = get_weather(CITY)
+    print_weather_data(weather_data, CITY)
