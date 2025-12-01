9f0003f72bedab089e3e62bd88c5285690876fd1
Piotr Moch <piotr@MacBook-Air-Piotr.local>
2025-12-01 23:20:57 +0100

    FastAPI implementation

--- src/main.py
+++ src/main.py
@@ -1,56 +1,14 @@
-import datetime as dt
-import requests
+from fastapi import FastAPI
+from utils import get_weather, format_weather_data
+from http import HTTPExc
 
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-API_KEY = open('key/api_key', 'r').read().strip()
 
-def city():
-    city_name = input("Choose city to check weather: ").strip()
-    if not city_name:
-        print("No city input!")
-        exit()
-    return city_name
+API_KEY = open('key/api_key').read().strip()
 
-def kelvin_to_celsius_fahrenheit(kelvin):
-    celsius = kelvin - 273.15
-    fahrenheit = celsius * (9/5) + 32
-    return celsius, fahrenheit
+app = FastAPI()
 
-def get_weather(city: str):
-    params = {
-        "appid": API_KEY,
-        "q": city
-    }
-    response = requests.get(BASE_URL, params=params)
-
-    if response.status_code != 200:
-        raise Exception(f"API returned status {response.status_code}: {response.text}")
-
-    return response.json()
-
-def print_weather_data(data, city):
-    temp_kelvin = data['main']['temp']
-    temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp_kelvin)
-
-    feels_like_kelvin = data['main']['feels_like']
-    feels_like_celsius, feels_like_fahrenheit = kelvin_to_celsius_fahrenheit(feels_like_kelvin)
-
-    humidity = data['main']['humidity']
-    description = data['weather'][0]['description']
-
-    sunrise_time = dt.datetime.utcfromtimestamp(data['sys']['sunrise'] + data['timezone'])
-    sunset_time = dt.datetime.utcfromtimestamp(data['sys']['sunset'] + data['timezone'])
-
-    print(f"Temperature in {city}: {temp_celsius:.2f}°C or {temp_fahrenheit:.2f}°F")
-    print(f"Temperature in {city} feels like: {feels_like_celsius:.2f}°C or {feels_like_fahrenheit:.2f}°F")
-    print(f"Humidity in {city}: {humidity}%")
-    print(f"The weather in {city}: {description}")
-    print(f"The sunrise in {city}: {sunrise_time}")
-    print(f"The sunset in {city}: {sunset_time}")
-
-# tylko tutaj wywołujemy input i API
-if __name__ == "__main__":
-    CITY = city()
-    print(f"DEBUG: Checking weather for '{CITY}'")
-    weather_data = get_weather(CITY)
-    print_weather_data(weather_data, CITY)
+@app.get("/weather/{city}")
+def weather(city: str):
+    data = get_weather(city, API_KEY)
+    formatted = format_weather_data(data, city)
+    return formatted
---
+++ src/utils.py
@@ -0,0 +1,47 @@
+import datetime as dt
+import requests
+
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+
+def get_weather(city: str, api_key):
+    params = {
+        "appid":  api_key,
+        "q": city
+    }
+    response = requests.get(BASE_URL, params=params)
+
+    if response.status_code != 200:
+        raise Exception(f"API returned status {response.status_code}: {response.text}")
+
+    return response.json()
+
+def kelvin_to_celsius_fahrenheit(kelvin):
+    celsius = kelvin - 273.15
+    fahrenheit = celsius * (9/5) + 32
+    return celsius, fahrenheit
+
+def format_weather_data(data, city):
+    temp_kelvin = data['main']['temp']
+    temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp_kelvin)
+
+    feels_like_kelvin = data['main']['feels_like']
+    feels_like_celsius, feels_like_fahrenheit = kelvin_to_celsius_fahrenheit(feels_like_kelvin)
+
+    humidity = data['main']['humidity']
+    description = data['weather'][0]['description']
+
+    sunrise_time = str(dt.datetime.utcfromtimestamp(data['sys']['sunrise'] + data['timezone']))
+    sunset_time = str(dt.datetime.utcfromtimestamp(data['sys']['sunset'] + data['timezone']))
+
+
+    return {
+        "city": city,
+        "Temperature_c": temp_celsius,
+        "Temperature_f": temp_fahrenheit,
+        "Feels_like_c": feels_like_celsius,
+        "Feels_like_f": feels_like_fahrenheit,
+        "Humidity": humidity,
+        "Description": description,
+        "Sunrise": sunrise_time,
+        "Sunset": sunset_time,
+    }
